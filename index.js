959a029023cc1c6e0efdf6c5c5f9c8d9b399825a
Afeni Samuel Oluwatimilehin <144561797+TimTech5@users.noreply.github.com>
2024-07-01 16:32:29 -0700

    Initializing Project

---
+++ index.js
@@ -0,0 +1,18 @@
+let x = 2;
+var bulb;
+
+switch (x) {
+  case 0:
+    bulb = "Off";
+    console.log(bulb);
+    break;
+
+  case 1:
+    bulb = "On";
+    console.log(bulb);
+    break;
+
+  default:
+    bulb = "Value Not Found";
+    console.log(bulb);
+}
